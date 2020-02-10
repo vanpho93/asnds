14a94347ebb524ded57a224afc2729a35968798a
Pho Nguyen <pho.nguyenvan@nashtechglobal.com>
2020-02-10 23:43:12 +0700

    merge sort

---
+++ src/algorithms/sort/selection-sort/selection-sort.test.ts
@@ -0,0 +1,12 @@
+import { deepEqual } from 'assert'
+import { getTestTitle } from '../../../helpers'
+import { selectionSort } from './selection-sort'
+
+const TEST_TITLE = getTestTitle(__filename)
+
+describe(TEST_TITLE, () => {
+  it(`${TEST_TITLE} Can use bubble sort`, () => {
+    const result = selectionSort([1, 4, 2, 5, 3, 9, 2, 1])
+    deepEqual(result, [ 1, 1, 2, 2, 3, 4, 5, 9 ])
+  })
+})
