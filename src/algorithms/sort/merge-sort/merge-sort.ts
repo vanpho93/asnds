f4df51799919a40824d7d9d3d888dd4c3d88a1bc
Pho Nguyen <pho.nguyenvan@nashtechglobal.com>
2020-02-22 00:46:49 +0700

    Add merge sort interative

--- src/algorithms/sort/merge-sort/merge-sort.ts
+++ src/algorithms/sort/merge-sort/merge-sort.ts
@@ -8,6 +8,25 @@ export function mergeSortRecursive(input: number[]): number[] {
   return merge(mergeSortRecursive(leftArray), mergeSortRecursive(rightArray))
 }
 
+export function mergeSortIterative(input: number[]): number[] {
+  const totalLevel = Math.floor(Math.log2(input.length)) + 1
+  for (let i = 1; i <= totalLevel; i++) {
+    const childArrayLength = Math.pow(2, i - 1) // 1 -> 2 -> 4 -> 8 -> ...
+    for (let j = 0; j < input.length; j += childArrayLength * 2) {
+      const leftArrayStart = j
+      const leftArrayEnd = j + childArrayLength - 1
+      const rightArrayStart = leftArrayEnd + 1
+      const rightArrayEnd = rightArrayStart + childArrayLength - 1
+
+      const leftArray = input.slice(leftArrayStart, leftArrayEnd + 1)
+      const rightArray = input.slice(rightArrayStart, rightArrayEnd + 1)
+      const mergedArray = merge(leftArray, rightArray)
+      mergedArray.forEach((value, index) => input[index + j] = value)
+    }
+  }
+  return input
+}
+
 function merge(leftArray: number[], rightArray: number[]): number[] {
   const result = []
 
@@ -28,8 +47,4 @@ function merge(leftArray: number[], rightArray: number[]): number[] {
     }
   }
   return result
-}
-
-export function mergeSortIterative(input: number[]): number[] {
-  return []
 }
--- src/algorithms/sort/merge-sort/merge.test.ts
+++ src/algorithms/sort/merge-sort/merge.test.ts
@@ -1,11 +1,15 @@
 import { ok } from 'assert'
 import { getTestTitle, isValidSortFunction } from '../../../helpers'
-import { mergeSortRecursive } from './merge-sort'
+import { mergeSortRecursive, mergeSortIterative } from './merge-sort'
 
 const TEST_TITLE = getTestTitle(__filename)
 
 describe(TEST_TITLE, () => {
-  it(`${TEST_TITLE} Can use merge sort`, () => {
+  it(`${TEST_TITLE} Can use merge sort recursive`, () => {
     ok(isValidSortFunction(mergeSortRecursive))
+  })
+
+  it(`${TEST_TITLE} Can use merge sort iterative`, () => {
+    ok(isValidSortFunction(mergeSortIterative))
   })
 })
