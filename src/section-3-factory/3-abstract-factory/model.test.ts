d6b8877498f8f3e3b19c17abe113f5a48fbef133
Brian Nguyen <vanpho01@gmail.com>
2020-03-26 21:12:53 +0700

    Abstract Factory

---
+++ src/section-3-factory/3-abstract-factory/model.test.ts
@@ -0,0 +1,29 @@
+import td from 'testdouble'
+import { HotDrinkMachine } from './model'
+import { TestHelper } from '../../helpers'
+import { deepEqual } from 'assert'
+
+const TEST_TITLE = TestHelper.getTestTitle(__filename)
+
+describe(TEST_TITLE, () => {
+  beforeEach(TEST_TITLE, () => {
+    td.replace(console, 'log', td.function())
+  })
+
+  afterEach(TEST_TITLE, () => {
+    td.reset()
+  })
+
+  it(`${TEST_TITLE} HotDrinkMachine works`, () => {
+    const hotDrinkMachine = new HotDrinkMachine()
+    const coffee = hotDrinkMachine.makeDrink('COFFEE', 100)
+    coffee.consume()
+    td.verify(console.log('Grind some beans, boil water, pour 100ml'))
+    td.verify(console.log('This coffee is delicious!'))
+
+    const tea = hotDrinkMachine.makeDrink('TEA', 100)
+    tea.consume()
+    td.verify(console.log('Put in tea bag, boil water, pour 100ml'))
+    td.verify(console.log('This tea is nice with lemon!'))
+  })
+})
---
+++ src/section-3-factory/3-abstract-factory/model.ts
@@ -0,0 +1,59 @@
+import { isNil } from 'lodash'
+
+interface IHotDrink {
+  consume(): void
+}
+
+class Tea implements IHotDrink {
+  consume() {
+    console.log('This tea is nice with lemon!')
+  }
+}
+
+class Coffee implements IHotDrink {
+  consume() {
+    console.log('This coffee is delicious!')
+  }
+}
+
+abstract class HotDrinkFactory<T> {
+  abstract prepare(amount: number): T
+}
+
+class TeaFactory extends HotDrinkFactory<Tea> {
+  prepare(amount: number) {
+    console.log(`Put in tea bag, boil water, pour ${amount}ml`);
+    return new Tea();
+  }
+}
+
+class CoffeeFactory extends HotDrinkFactory<Coffee> {
+  prepare(amount: number) {
+    console.log(`Grind some beans, boil water, pour ${amount}ml`);
+    return new Coffee();
+  }
+}
+
+type ClassOfHotDrinkFactory = { new (): HotDrinkFactory<IHotDrink> }
+
+const availableDrinks: { [key: string]: ClassOfHotDrinkFactory } = {
+  'TEA': TeaFactory,
+  'COFFEE': CoffeeFactory,
+}
+
+export class HotDrinkMachine {
+  private factories: { [key: string ]: HotDrinkFactory<IHotDrink> }
+  constructor() {
+    this.factories = {};
+    for (let drink in availableDrinks)
+    {
+      this.factories[drink] = new availableDrinks[drink]()
+    }
+  }
+
+  makeDrink(type: string, amount: number) {
+    const factory = this.factories[type]
+    if (isNil(factory)) throw new Error(`Don't know how to make ${type}`)
+    return factory.prepare(amount);
+  }
+}
